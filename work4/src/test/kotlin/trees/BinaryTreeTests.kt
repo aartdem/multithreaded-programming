8a5de7ac4ce81ed8ffc0ee4fc22c12cf09a184a1
Artem Demchenko <artdem243@gmail.com>
2024-05-28 01:30:03 +0300

    Fix coroutines

--- work4/src/test/kotlin/trees/BinaryTreeTests.kt
+++ work4/src/test/kotlin/trees/BinaryTreeTests.kt
@@ -1,7 +1,6 @@
 package trees
 
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import kotlin.random.Random
@@ -15,39 +14,41 @@ class BinaryTreeTests {
         tree = CoarseGrainedBinaryTree()
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class, DelicateCoroutinesApi::class)
     @Test
     fun addingValuesTwoThreadsTest() {
         val valuesToAdd1 = List(1000) { Random.nextInt(1000) }
         val valuesToAdd2 = List(1000) { Random.nextInt(1000) }
         runBlocking {
-            launch {
+            launch(newSingleThreadContext("Thread1")) {
                 valuesToAdd1.forEach { tree.add(it) }
             }
-            launch {
+            launch(newSingleThreadContext("Thread2")) {
                 valuesToAdd2.forEach { tree.add(it) }
             }
         }
         Assertions.assertEquals(tree.getValues(), (valuesToAdd1 + valuesToAdd2).sorted().distinct())
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class, DelicateCoroutinesApi::class)
     @Test
     fun deletingValuesTwoThreadsTest() {
         val valuesToRemove1 = List(1000) { Random.nextInt(1000) }
         val valuesToRemove2 = List(1000) { Random.nextInt(1000) }
         runBlocking {
-            val j1 = launch {
+            val j1 = launch(newSingleThreadContext("Thread1")) {
                 valuesToRemove1.forEach { tree.add(it) }
             }
-            val j2 = launch {
+            val j2 = launch(newSingleThreadContext("Thread2")) {
                 valuesToRemove2.forEach { tree.add(it) }
             }
             j1.join()
             j2.join()
 
-            launch {
+            launch(newSingleThreadContext("Thread1")) {
                 valuesToRemove1.forEach { tree.remove(it) }
             }
-            launch {
+            launch(newSingleThreadContext("Thread2")) {
                 valuesToRemove2.forEach { tree.remove(it) }
             }
         }
